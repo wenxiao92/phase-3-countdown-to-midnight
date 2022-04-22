595f7f04c76c77e2ace61b419c8c4d217a71a1c8
Wen Xiao <wennstyle@gmail.com>
2022-04-22 15:52:56 -0400

    commit tree e06b291fa3b2b7ee308feb69ef02c2503726993f

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(x)
+
+    while x > 0
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(y)
+    while y>0
+        puts "#{y} SECOND(S)!"
+        sleep(1)
+        y -= 1
+    end
+
+    return "HAPPY NEW YEAR!"
 end
